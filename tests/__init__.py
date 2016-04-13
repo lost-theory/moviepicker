b95116bb42dee52f71d6cd5db8675fcbb67c9c51
Steven Kryskalla <skryskalla@gmail.com>
2016-04-13 02:13:03 -0700

    tests: add a mocked test

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,8 +10,11 @@ import unittest
 
 os.environ['DBURI'] = "sqlite://" #empty URL means using an in-memory DB
 
+from mock import patch, mock_open
+
 from app import app, db
 from app import User
+from movies import fetch_wikipedia_titles
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
@@ -19,6 +22,23 @@ class AppTestCase(unittest.TestCase):
         with app.app_context():
             db.create_all()
         self.client = app.test_client()
+
+## non-app tests ##############################################################
+
+class MoviePickerTests(unittest.TestCase):
+    urlopen = mock_open(read_data='{"query": {"categorymembers": [{"title": "Up"}]}}')
+
+    @patch("urllib.urlopen", urlopen)
+    def test_fetch_wikipedia_titles(self):
+        titles = fetch_wikipedia_titles("Pixar_animated_films")
+        assert len(self.urlopen.mock_calls) == 2
+        assert "https://en.wikipedia.org/" in str(self.urlopen.mock_calls[0])
+        assert "Pixar_animated_films" in str(self.urlopen.mock_calls[0])
+        assert "read()" in str(self.urlopen.mock_calls[1])
+        assert len(titles) == 1
+        assert titles[0] == 'Up'
+
+## app tests ##################################################################
 
 class ViewTests(AppTestCase):
     def test_index_logged_out(self):
@@ -38,4 +58,3 @@ class ModelTests(AppTestCase):
             assert u.username == "test"
             assert u.email == "test@wow.com"
             assert u.password_hash != "asdfasdf"
-
