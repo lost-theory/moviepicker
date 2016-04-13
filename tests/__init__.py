111ccd257d35b211b1d3ae319e73e851fafc43a2
Steven Kryskalla <skryskalla@gmail.com>
2016-04-13 01:36:17 -0700

    tests: add a unit test suite

---
+++ tests/__init__.py
@@ -0,0 +1,42 @@
+'''
+To run tests:
+
+$ ~/mp_app_env/bin/python -m unittest tests
+..
+----------------------------------------------------------------------
+Ran 2 tests in 0.051s
+'''
+
+import os
+import unittest
+
+os.environ['DBURI'] = "sqlite://" #empty URL means using an in-memory DB
+
+from app import app, db
+from app import User
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        with app.app_context():
+            db.create_all()
+        self.client = app.test_client()
+
+class ViewTests(AppTestCase):
+    def test_index_logged_out(self):
+        res = self.client.get('/')
+        assert res.status == '200 OK'
+        assert "<strong>Register</strong> or <strong>Log in</strong>" in res.data
+
+    def test_add_cat_logged_out_redirect(self):
+        res = self.client.get('/categories')
+        assert res.status == '302 FOUND'
+        assert '/login' in res.headers['Location']
+
+class ModelTests(AppTestCase):
+    def test_user_create(self):
+        with app.app_context():
+            u = User.create("test", "test@wow.com", "asdfasdf", "asdfasdf")
+            assert u.username == "test"
+            assert u.email == "test@wow.com"
+            assert u.password_hash != "asdfasdf"
